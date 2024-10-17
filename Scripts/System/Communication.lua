a9fb1c9d24eaf123f1d4eebd5f2f4f85dc375463
VinzentObuun <bvlgray@gmail.com>
2024-10-16 22:14:29 -0400

    Create Communication.lua

---
+++ Scripts/System/Communication.lua
@@ -0,0 +1,20 @@
+createConsole("chatConsole", 8, 55, 38, 1480, 369) -- This and the rest below are regarding communication capture. Yes, I am pixel specific. It is dumb.
+  
+function onProtocolEnabled(_, protocol)
+  if protocol == "GMCP" then
+    sendGMCP([[ Core.Supports.Add ["Comm.Channel 1"] ]])
+  end
+end
+  
+registerAnonymousEventHandler("sysProtocolEnabled", "onProtocolEnabled")
+  
+function digestChat()
+  local ANSI, MXP = rex.new("\\e\\[(\\d+;)*\\d+m"), rex.new("\\e\\[4z\\x3\.+?\\x4")
+  local text = rex.gsub(ansi2decho(gmcp.Comm.Channel.Text.text), MXP, "")
+  
+  if not gmcp.Comm.Channel.Text.channel:find("Homunculus") then -- Easy to add exceptions for spam from denizens, mobs, people, etc. Below is the only one I have at this time.
+		decho("chatConsole", "<255,255,255:0,0,0>- "..text.."\n")
+	end
+end
+  
+registerAnonymousEventHandler("gmcp.Comm.Channel.Text", "digestChat")
